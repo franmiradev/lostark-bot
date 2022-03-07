c2f1619b09169e892f4b4453fbb8caae37815374
Francisco Jose Mira Moya <fmiramoy@everis.com>
2022-03-07 20:47:21 +0100

    Method to generate bottom

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 
+
+
 window = tk.Tk()
 window.tk.call("source", "azure.tcl")
 window.tk.call("set_theme", "dark")
@@ -21,21 +23,14 @@ def main_app():
 def createWindows():
     tab_control = ttk.Notebook(window)
     # must keep a global reference to these two
-    
+    archerImage = ImageTk.PhotoImage(Image.open('images/archer.png').resize((200, 200), Image.ANTIALIAS))
     # BUYER TAB
     buyerTab = ttk.Frame(tab_control)
     tab_control.add(buyerTab, text='Buy items')
     mailTab = ttk.Frame(tab_control)
     tab_control.add(mailTab, text='Clear mail box')
-    
-    archerImage = ImageTk.PhotoImage(Image.open('images/archer.png').resize((200, 200), Image.ANTIALIAS))
-    archerLabel = tk.Label(buyerTab,
-                        justify='center', 
-                        font=("-size", 10, "-weight", "bold"),
-                        image=archerImage)
-    archerLabel.grid(row=5, column=0, pady=40, columnspan=1)
-    archerLabel.place(relx=0.5, rely=0.85, anchor='center')
-    archerLabel.image=archerImage
+    noAfkTab = ttk.Frame(tab_control)
+    tab_control.add(noAfkTab, text='No AFK')
     
     buyerLabel = tk.Label(buyerTab, text= 'OPEN THE AUCTION AT THE GAME',
                         justify='center', 
@@ -61,11 +56,7 @@ def createWindows():
     boton.grid(row=2, column=0, padx=5, pady=10, sticky="nsew")
     boton.place(relx=0.5, rely=0.5, anchor='center')
     
-    rememberLabel = tk.Label(buyerTab, text= 'IMPORTANT: Remember, press \'Ctrl\' until bot stop \n when is running',
-                        anchor='center', 
-                        font=("-size", 8, "-weight", "bold"))
-    rememberLabel.grid(row=5, column=0, pady=10, columnspan=2)
-    rememberLabel.place(relx=0.5, rely=0.85, anchor='center')
+    bottomLabel(buyerTab)
     
     # MAILBOX    
     mailLabel = tk.Label(mailTab, text= 'OPEN THE MAILBOX AT THE GAME', 
@@ -78,17 +69,24 @@ def createWindows():
     clearBoton.grid(row=2, column=0, padx=5, pady=10, sticky="nsew")
     clearBoton.place(relx=0.5, rely=0.5, anchor='center')
     
-    archerLabel = tk.Label(mailTab,
-                        justify='center', 
-                        font=("-size", 10, "-weight", "bold"),
-                        image=archerImage)
+    bottomLabel(mailTab)
+
+    bottomLabel(noAfkTab)
+
+    tab_control.pack(fill = "both", expand = "yes")
+
+
+def bottomLabel(tab):
+    archerImage = ImageTk.PhotoImage(Image.open('images/archer.png').resize((200, 200), Image.ANTIALIAS))
+    archerLabel = tk.Label(tab,
+                    justify='center', 
+                    font=("-size", 10, "-weight", "bold"),
+                    image=archerImage)
     archerLabel.grid(row=2, column=0, pady=10, columnspan=1)
     archerLabel.place(relx=0.5, rely=0.85, anchor='center')
     archerLabel.image=archerImage
-    rememberLabel = tk.Label(mailTab, text= 'IMPORTANT: Remember, press \'Ctrl\' until bot stop \n when is running',
+    rememberLabel = tk.Label(tab, text= 'IMPORTANT: Remember, press \'Ctrl\' until bot stop \n when is running',
                         justify='center', 
                         font=("-size", 8, "-weight", "bold"))
     rememberLabel.grid(row=2, column=0, pady=245, columnspan=2)
     rememberLabel.place(relx=0.5, rely=0.85, anchor='center')
-
-    tab_control.pack(fill = "both", expand = "yes")
