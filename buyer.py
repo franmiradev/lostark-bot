e90abc8d2577f9c261a714f756ee7dacb43e5552
Francisco Jose Mira Moya <fmiramoy@everis.com>
2022-03-11 20:41:28 +0100

    Enable all resolutions

--- buyer.py
+++ buyer.py
@@ -4,23 +4,30 @@ import random
 import time
 from tkinter import messagebox
 
-buy_button = { "x":2088, "y":1232}
-item = { "x":954, "y":442 }
-add = { "x":1376, "y":692}
-price = { "x":1356, "y":744 } 
-buy_final = { "x":1281, "y":1082 }
-confirm = { "x":1280, "y":780 }
-move_left = { "x":11, "y":579 }
-move_right = { "x":2540, "y":887 }
+res = pyautogui.size()
+w = res.width
+h = res.height
+
 minutes = 5
 seconds = 60*minutes
 
 def clic(position, times):
+    print(w)
+    print(h)
+    if w != 2560 or h != 1440:
+        position = map(position)
     pyautogui.moveTo(position["x"], position["y"],duration=0.1, tween=pyautogui.easeInOutQuad)
     for i in range(0, times):
         pyautogui.click()
     time.sleep(0)
-    
+
+def map(position):
+    qhd = { "x":2560, "y": 1440}
+    res_x, res_y = pyautogui.size()
+    position["x"] = (res_x * position["x"]) / qhd["x"]
+    position["y"] = (res_y * position["y"]) / qhd["y"]
+    return position
+
 def no_afk(mouseMove):
     if bool(random.getrandbits(1)):
         pyautogui.moveTo(move_right["x"], move_right["y"],duration=0.1, tween=pyautogui.easeInOutQuad)
@@ -34,8 +41,19 @@ def no_afk(mouseMove):
 
 def buy_item(entryPrice, amount, mouseMove):
     loop = True
+
     start = time.time() 
     while loop:
+
+        buy_button = { "x":2088, "y":1232}
+        item = { "x":954, "y":442 }
+        add = { "x":1376, "y":692}
+        price = { "x":1356, "y":744 } 
+        buy_final = { "x":1281, "y":1082 }
+        confirm = { "x":1280, "y":780 }
+        move_left = { "x":11, "y":579 }
+        move_right = { "x":2540, "y":887 }
+
         clic(item,2)
         clic(buy_button,1)
         clic(add,2)
--- mailbox.py
+++ mailbox.py
@@ -1,32 +1,18 @@
 import time
+from buyer import *
 import keyboard
 import pyautogui
-
-mess_x = 280
-mess_y = 139
-acc_x = 727
-acc_y = 677
-del_x = 865
-del_y = 673
-
-r = 50
-
-def clic(x,y,times):
-    #print(f"{x}, {y}")
-    pyautogui.moveTo(x, y,duration=0.2, tween=pyautogui.easeInOutQuad)
-    if times>1:
-        pyautogui.doubleClick()
-    else:
-        pyautogui.click()
-    time.sleep(0)
-
-
 
 def clearMail():
     loop = True
     while loop:
-        clic(mess_x, mess_y,1)
-        clic(acc_x, acc_y,1)
-        clic(del_x, del_y,1)
+        mailbox = { "x":280, "y":139 }
+        accept = { "x":727, "y":677 }
+        delete = { "x":865, "y": 673 }
+        clic(mailbox,1)
+        time.sleep(0.1)
+        clic(accept,1)
+        time.sleep(0.1)
+        clic(delete,1)
         if keyboard.is_pressed("ctrl"):
             loop = False
